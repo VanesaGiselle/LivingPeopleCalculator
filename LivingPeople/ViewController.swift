dfc6aa66aa7c8de53a803dc2fe5d0edb72e1c3f9
Vanesa Korbenfeld <vkorbenfeld@gmail.com>
2023-05-14 23:22:25 -0300

    Initial Commit

---
+++ LivingPeople/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LivingPeople
+//
+//  Created by Vanesa Korbenfeld on 14/05/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
